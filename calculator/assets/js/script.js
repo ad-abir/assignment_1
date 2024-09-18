4cd2b6b295e6d42dc6993c8718132d19d2ec0e3c
Abhishek <abhishek.das0023@gmail.com>
2024-09-19 01:11:06 +0600

    Updated

--- calculator/assets/js/script.js
+++ calculator/assets/js/script.js
@@ -1,17 +1,14 @@
 function calculate() {
-    // Retrieve input values and the selected operation
     const num1 = parseFloat(document.getElementById('num1').value);
     const num2 = parseFloat(document.getElementById('num2').value);
     const operation = document.getElementById('operation').value;
     let result;
   
-    // Error handling for invalid inputs
     if (isNaN(num1) || isNaN(num2)) {
       document.getElementById('resultValue').textContent = "Please enter valid numbers.";
       return;
     }
   
-    // Perform the operation
     switch(operation) {
       case 'add':
         result = num1 + num2;
@@ -33,7 +30,6 @@ function calculate() {
         result = "Invalid operation.";
     }
   
-    // Display the result
     document.getElementById('resultValue').textContent = result;
   }
   
